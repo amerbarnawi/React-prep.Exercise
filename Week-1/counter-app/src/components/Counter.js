2285f5cca711895340a52e8609e79941e92433bb
Amer <barnawi.amer@yahoo.com>
2022-09-18 13:53:34 +0200

    Going back to using a function for counter component instead of class.

--- Week-1/counter-app/src/components/Counter.js
+++ Week-1/counter-app/src/components/Counter.js
@@ -1,48 +1,34 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Button from "./Button";
 import Count from "./Count";
 
-class Counter extends Component {
-  constructor() {
-    super();
-    this.state = {
-      count: 0,
-    };
-    this.Add_1 = this.Add_1.bind(this);
-    this.Decrement_1 = this.Decrement_1.bind(this);
+function Counter() {
+  const [count, setCount] = useState(() => {
+    return 0;
+  });
+  const feedback = count > 10 ? "It's higher than 10!" : "Keep counting...";
+
+  function Add_1() {
+    setCount(count + 1);
   }
 
-  Add_1() {
-    this.setState((prevState) => {
-      return { count: prevState.count + 3 };
-    });
-  }
-
-  Decrement_1() {
-    if (this.state.count > 0) {
-      this.setState((prevState) => {
-        return { count: prevState.count - 3 };
-      });
+  function Decrement_1() {
+    if (count > 0) {
+      setCount(count - 1);
     }
   }
 
-  render() {
-    const buttonFunctions = {
-      increment: this.Add_1,
-      decrement: this.Decrement_1,
-    };
-
-    const feedback =
-      this.state.count > 10 ? "It's higher than 10!" : "Keep counting...";
-
-    return (
-      <div>
-        <Count value={this.state.count} />
-        <h1>{feedback}</h1>
-        <Button functions={buttonFunctions} />
-      </div>
-    );
-  }
+  const buttonFunctions = {
+    increment: Add_1,
+    decrement: Decrement_1,
+  };
+  return (
+    <div>
+      <Count value={count} />
+      <h1>{feedback}</h1>
+      <Button functions={buttonFunctions} />
+    </div>
+  );
 }
 
 export default Counter;
